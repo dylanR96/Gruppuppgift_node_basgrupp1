fa5237a2c6ba8ae02ca9ae0ea5fe99b76a72e934
dylanR96 <dylan.ravenson@gmail.com>
2024-06-02 21:30:32 +0200

    updated createOrder and getOrderStatus, unique id and delivey time is added to object, create seperate file to handle delivery time calculation, unique id is yet to be fully unique

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,58 +1,49 @@
 import db from "../db/database.js";
+import createDeliveryTime from "../services/createDeliveryTime.js";
 
 //Unique order ID (Math random kontrolleras )
 //Order time 15 min - 45 min (Math random)
 
 const createOrder = async (req, res) => {
-  let newOrder = req.body;
+  const orderId = Math.floor(Math.random() * (999 - 100) + 100);
+  let myOrder = orderId.toString();
 
-  function generateId() {
-    const timestamp = Date.now().toString();
-    const randomNum = Math.floor(Math.random() * (999 - 100) + 100);
-  }
-  const orderId = generateId();
+  const newOrder = req.body;
 
-  for (let order of newOrder) {
-    const { id, title, desc, price } = order;
-    if (id == null || title == null || desc == null || price == null) {
-      return res
-        .status(400)
-        .json({ error: "Each order must contain id, title, desc, and price" });
+  if (Array.isArray(newOrder)) {
+    for (let order of newOrder) {
+      const { id, title, desc, price } = order;
+      if (id == null || title == null || desc == null || price == null) {
+        return res.status(400).json({
+          error: "Each order must contain id, title, desc, and price",
+        });
+      }
     }
   }
 
+  newOrder.estDelivery = createDeliveryTime();
+
   try {
-    db["order"].insert(newOrder);
-    return res.status(201).json(`Your order id: ${orderId}`);
+    db["order"].insert({ orderId: myOrder, ...newOrder });
+    return res.status(201).json(`Your order id: ${myOrder}`);
   } catch (error) {
+    console.log(error);
     return res.status(500).send({ error: "Error adding new order" });
   }
 };
 
 const getOrderStatus = async (req, res) => {
-  const orderId = req.params.orderId;
-
-  const orderTime = Math.floor(Math.random() * (45 - 25 + 1) + 25);
-  const currentDate = new Date();
-  const hour = currentDate.getHours();
-  const min = currentDate.getMinutes();
-
-  let deliveryHour = hour;
-  let deliveryMin = min + orderTime;
-
-  if (deliveryMin >= 60) {
-    deliveryHour += Math.floor(deliveryMin / 60);
-    deliveryMin %= 60;
-  }
-
-  const deliveryTime = `${deliveryHour}:${
-    deliveryMin < 10 ? "0" + deliveryMin : deliveryMin
-  }`;
-
-  console.log(deliveryTime);
+  const { orderId } = req.params;
+  console.log(orderId);
 
   try {
-    const orderData = await db["order"].findOne({ order: orderId });
+    const orderData = await db["order"].findOne({ orderId: orderId });
+    if (!orderData) {
+      return res.status(404).send({ error: "Order not found" });
+    }
+    return res.status(200).json({
+      message: `Your estimated delivery time is ${orderData.estDelivery}`,
+    });
   } catch (error) {
     return res.status(500).send({ error: "Error finding order id" });
   }
