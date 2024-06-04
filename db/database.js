cb6030a7edcc12bd4b6a181ab5c3474901321f54
dylanR96 <dylan.ravenson@gmail.com>
2024-06-04 10:06:33 +0200

    Changed database.js to facilitate Os  operating systems as well as windows.

--- db/database.js
+++ db/database.js
@@ -2,13 +2,14 @@ import nedb from "nedb-promises";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
 const database_names = ["company", "order", "users"];
 const db = {};
+
 database_names.forEach((name) => {
-  const database_directory = `${__dirname}\\${name}.db`;
+  const database_directory = path.join(dirname, `${name}.db`);
   db[name] = new nedb({
     filename: database_directory,
     autoload: true,
